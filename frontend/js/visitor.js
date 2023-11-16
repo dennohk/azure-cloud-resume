f0d60fa7ede7e6a23c7311b3ded475bb4a612ef2
Dennis Mutua <dkimeu.mutua@gmail.com>
2023-11-16 13:23:52 +0300

    Modified Javascript code

---
+++ frontend/js/visitor.js
@@ -0,0 +1,14 @@
+//Azure Fuction endpoint
+const functionUrl ="";
+
+//Get request to Azure endpoint
+function updateVisitorCount() {
+    fetch(functionUrl).then((response) => response.json())
+    .then((count) => {
+        document.getElementById(webcounter).textContent = count;
+    })
+    .catch((error) =>{
+        console.log("Error: " +error);
+    });
+};
+updateVisitorCount()
